20eaaddd449c145ed14aa043d3725f34ecf64b36
Ali ZAMAN <zamanmuhammadali@gmail.com>
2024-09-30 23:23:41 +0200

    feat: add project section in portfolio (#13)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { NavBar } from './components/nav-bar';
 import { Banner } from './components/banner';
 import { Skills } from './components/skills';
+import { Projects } from './components/Projects'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Banner />
         <Skills />
+        <Projects />
       </div>
     </>
   );

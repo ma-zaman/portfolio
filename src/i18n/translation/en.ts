ac29a4cf5d845be7b856a4ce3ef64d498c3340a3
Ali ZAMAN <zamanmuhammadali@gmail.com>
2024-09-30 23:22:18 +0200

    feat: add project section in portfolio

--- src/i18n/translation/en.ts
+++ src/i18n/translation/en.ts
@@ -16,4 +16,34 @@ export const en = {
     subtitle:
       'Learn more about my Fullstack development skills: NodeJS, Vue3, React, MongoDB, TypeScript...',
   },
+  professional_projects: {
+    title: 'Professional projects',
+    lipn_intern: {
+      title: 'Intern (3 months)',
+      description: "End-of-course internship during which I took part in the creation of a model and the front-end development of some of the model's screens using Vue"
+    },
+    cnrs_apprenticeship: {
+      title: 'Apprentice (3 years)',
+      description: "Mock-up creation, fullstack development in JavaScript with Vue/ Node/ Express/ MongoDB for the redesign of a research laboratory's intranet. Intern supervision and client presentation"
+    },
+    capgemini: {
+      title: 'Software engineer (Apr-2023/ Currently)',
+      description: "Software development consultant specializing in fullstack development with JavaScript/ TypeScript under various frameworks"
+    },
+    orange_first: {
+      title: 'Software engineer (Sept-2023/ Dec-2023)',
+      description: "Frontend development with TypeScript and Vue3 for a management application"
+    },
+    orange_second: {
+      title: 'Software engineer (Jan-2024/ Currently)',
+      description: "Fullstack development with TypeScript/ React/ Nest/ MongoDB/ Kafka for an internal application"
+    }
+  },
+  personal_projects: {
+    title: 'Personal projects',
+    portfolio: {
+      title: 'Portfolio',
+      description: "Creating a portfolio with TypeScript and React and creating a deployment pipeline on an FTP server"
+    }
+  }
 };
